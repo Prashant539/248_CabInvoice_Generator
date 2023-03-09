2cb014d826fd3ca1e5ad821dc5a02495550f3466
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-09 16:35:08 +0530

    [Prashant]Add: Calculate Ride Fare for Multiple rides

--- CabInvoiceGenerator/TestProject1/UnitTest1.cs
+++ CabInvoiceGenerator/TestProject1/UnitTest1.cs
@@ -22,5 +22,19 @@ namespace TestProject1
             double expected = 60.0;
             Assert.AreEqual(expected, fare);
         }
+        [Test]
+        /// <summary>
+        /// UC2-Returns the totlfare for multiple rides
+        /// </summary>
+
+        public void Return_Multiple_Rides_TotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.totalFare, expected.totalFare);
+        }
     }
 }
